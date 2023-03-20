da45c9d771f122cee499ecfe0420cc0104ad35b4
H2ZTest <128400427+H2ZTest@users.noreply.github.com>
2023-03-20 19:27:26 +0400

    Create ISolo.sol

---
+++ Assignment 2/RockPaperScissors/interfaces/ISolo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+interface ISolo{
+
+    function createSoloGameBNB(address user, uint answer, uint bet)external  returns(uint gameId);
+
+    function finishSoloGameBNB(address user, uint gameId)external returns(address winner, uint bet);
+
+    function createSoloGameToken(address user, uint answer, address token, uint amount)external returns(uint gameId);
+
+    function finishSoloGameToken(address user, uint gameId)external returns(address winner, address token, uint bet);
+
+}
+
